fe3c01bbf7fd24c06cbe903769b95ce2a0bc5e1f
11andreykalina <11andreykalina@gmail.com>
2025-08-11 15:47:39 +0300

    wrapermobail

---
+++ index.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.swiper-contianer', {
+   slidesOffsetBefore: 16,
+   slidesPerView: 1.3,
+   spaceBetween: 32,
+   slidesOffsetAfter: 16,
+  centeredSlidesBounds: true, // Ограничение, чтобы активный слайд не выходил за границы
+
+
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    
+  },
+
+
+});
