f4e6844092f173e384c07f94ac1357e65500b5a9
11andreykalina <11andreykalina@gmail.com>
2025-08-11 23:18:07 +0300

    wraperv3

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-const swiper = new Swiper('.swiper-contianer', {
-   slidesOffsetBefore: 16,
-   slidesPerView: 1.3,
-   spaceBetween: 32,
-   slidesOffsetAfter: 16,
-   centeredSlidesBounds: true, // Ограничение, чтобы активный слайд не выходил за границы
+document.addEventListener('DOMContentLoaded', function() {
+    const swiper = new Swiper('.swiper-container', { // Исправлено название класса
+        slidesOffsetBefore: 16,
+        slidesPerView: 1.3,
+        spaceBetween: 32,
+        slidesOffsetAfter: 16,
+        centeredSlidesBounds: true, // Ограничение, чтобы активный слайд не выходил за границы
 
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
 
+    const toggleButton = document.getElementById('toggleButton');
+    const gridItems = document.querySelectorAll('.swiper-slide');
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    
-  },
+    let isExpanded = false;
 
-
+    toggleButton.addEventListener('click', () => {
+        if (isExpanded) {
+            gridItems.forEach((item, index) => {
+                if (index >= 6) {
+                    item.classList.add('hidden'); // Скрываем элементы начиная с третьего
+                }
+            });
+            toggleButton.textContent = 'Показать всё'; // Меняем текст на кнопке
+        } else {
+            gridItems.forEach(item => {
+                item.classList.remove('hidden'); // Показываем все элементы
+            });
+            toggleButton.textContent = 'Скрыть'; // Меняем текст на кнопке
+        }
+        isExpanded = !isExpanded; // Переключаем состояние
+    });
 });
